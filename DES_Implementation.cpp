870cc8f71054e5e6f4bbc85c64a0c467f6042346
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-06-23 17:15:53 -0400

    Added in header and necessary libraries

---
+++ DES_Implementation.cpp
@@ -0,0 +1,12 @@
+//Tahim Bhuiya
+//Assignment 2
+//03/04/2024
+
+
+#include <iostream>
+#include <bitset>
+#include <string>
+using namespace std;
+
+bitset<64> key;                // Key for encryption/decryption
+bitset<48> sub_key[16];         // Array to store 16 subkeys
