f9a8bd1dc6aa5d50df6ec621f7a7212ed21bcd2d
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-06-25 17:48:30 -0400

    Added in better comments for the expansion table

--- DES_Implementation.cpp
+++ DES_Implementation.cpp
@@ -76,15 +76,22 @@ int pc_2[] = {
 int shift_bits[] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
 
-// Expansion table for the round function
-int e[] = {32,  1,  2,  3,  4,  5,
-           4,  5,  6,  7,  8,  9,
-           8,  9, 10, 11, 12, 13,
-          12, 13, 14, 15, 16, 17,
-          16, 17, 18, 19, 20, 21,
-          20, 21, 22, 23, 24, 25,
-          24, 25, 26, 27, 28, 29,
-          28, 29, 30, 31, 32,  1};
+// Expansion (E) table used in the DES round function (f-function):
+// This table expands the 32-bit right half of the data to 48 bits by 
+// duplicating and reordering specific bits. This expansion allows mixing 
+// with the 48-bit subkey using XOR.
+// It also ensures that some bits are used more than once, enhancing diffusion.
+int e[] = {
+    32,  1,  2,  3,  4,  5,
+     4,  5,  6,  7,  8,  9,
+     8,  9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32,  1
+};
+
 
 // S-boxes for the round function
 int s_box[8][4][16] = {
